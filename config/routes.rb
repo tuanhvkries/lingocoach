45389a285e733a49a5c15c27a7b43fad819bdad4
Tu Anh von Kries <tuanhvkries@gmail.com>
2026-01-20 12:04:25 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  resources :exercises, only: [:index, :show] do
+    resources :chats, only: [:create]
+  end
+
+  resources :chats, only: [:index, :show] do
+    resources :messages, only: [:create]
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
