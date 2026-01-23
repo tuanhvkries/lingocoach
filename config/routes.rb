48e2cb5c3e54b53bf72992df816a873484d5dd17
Tu Anh von Kries <tuanhvkries@gmail.com>
2026-01-23 16:45:49 +0100

    Bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,17 @@
 Rails.application.routes.draw do
-  get 'messages/create'
-  get 'chat/new'
-  get 'chat/create'
-  get 'chat/show'
-  get 'exercises/index'
-  get 'exercises/show'
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
 
   resources :exercises, only: [:index, :show, :new, :create, :destroy] do
-    resources :chats, only: [:create, :destroy]
+    resources :chats, only: [:create]
   end
 
   resources :chats, only: [:index, :show, :destroy] do
     resources :messages, only: [:create]
   end
 
+  resources :messages, only: [:destroy]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
